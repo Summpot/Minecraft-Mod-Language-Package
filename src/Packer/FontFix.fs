a9af1e8714eb86fc3f42ba505aed81ce975c51d9
Summpot <summpot@outlook.com>
2021-01-01 16:00:00 +0800

    test push

---
+++ src/Packer/FontFix.fs
@@ -0,0 +1,21 @@
+﻿
+module FontFix 
+    let replaceWithTwoCharactor (input : char) =
+        match input with 
+        |'\ue900'->"钅卢"
+        |'\ue901' ->"钅杜"
+        |'\ue902'->"钅喜"
+        |'\ue903'->"钅波"
+        |'\ue904'->"钅黑"
+        |'\u9fcf'  ->"钅麦"
+        |'\ue906'->"钅达"
+        |'\ue907'->"钅仑"
+        |'\u9fd4' ->"钅哥"
+        |'\u9fed' ->"钅尔"
+        |'\ue90a'->"钅夫"
+        |'\ue90c'->"钅立"
+        |'\u9fec' ->"石田"
+        |'\u9feb' ->"奥气"
+        |_->input.ToString()
+
+
---
+++ src/Packer/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open System.Net.Http
+open System.IO.Compression
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let externalFiles =[
+        "LICENSE"
+        "pack.mcmeta"
+        "pack.png"
+        "README.md"
+    ]
+    //对每个版本生成一个包
+    let versions = 
+        Directory.GetDirectories("./projects","*",SearchOption.TopDirectoryOnly) 
+        |> Seq.map (fun p -> Path.GetRelativePath("./projects",p))
+    printfn "%A" versions
+    //let zipFileStream = File.Create("./Minecraft-Mod-Language-Package.zip")
+    //let archive = new ZipArchive(zipFileStream,ZipArchiveMode.Update)
+    
+    0 // return an integer exit code
